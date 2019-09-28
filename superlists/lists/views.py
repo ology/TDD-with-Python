e4618be339f16724b772da5546029e4751e73833
Gene Boggs <gene@ology.net>
2019-09-28 11:59:23 -0700

    Add an item_text!

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, 'home.html', {
-        'new_item_text': request.POST.get('id_new_item', ''), }
+        'new_item_text': request.POST.get('item_text', ''), }
     )
