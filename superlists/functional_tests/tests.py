7fe2fa97089f42732e037346aec7464f11000279
Gene Boggs <gene@ology.net>
2019-10-04 10:01:42 -0700

    Replace explicit sleep with implicit wait

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -4,8 +4,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import unittest
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -22,10 +25,17 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(2)
 
     def check_row(self, item_num, item_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn(str(item_num) + ': ' + item_text, [row.text for row in rows])
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(str(item_num) + ': ' + item_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_page(self):
         # Visit the URL
