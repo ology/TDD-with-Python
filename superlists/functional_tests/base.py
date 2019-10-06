ae2ecf793e1a9a57396ee10ad2d64cd10bad5a57
Gene Boggs <gene@ology.net>
2019-10-06 10:43:44 -0700

    Separate functional tests

---
+++ superlists/functional_tests/base.py
@@ -0,0 +1,33 @@
+import os
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+import time
+
+MAX_WAIT = 10
+
+class FunctionalTest(StaticLiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def input_keys(self, inputbox, item_text):
+        inputbox.send_keys(item_text)
+        inputbox.send_keys(Keys.ENTER)
+
+    def check_row(self, item_num, item_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(str(item_num) + ': ' + item_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
---
+++ superlists/functional_tests/test_item_validation.py
@@ -0,0 +1,9 @@
+from .base import FunctionalTest
+from unittest import skip
+
+class LayoutTest(FunctionalTest):
+
+    @skip
+    def test_cannot_add_empty(self):
+        # TODO
+        self.fail('TODO')
