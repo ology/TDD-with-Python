3c27f4676a397e9968a26c01eb59479534232665
Gene Boggs <gene@ology.net>
2019-10-08 09:22:38 -0700

    No skipping. Look for correct css class. Check for correct row number

--- superlists/functional_tests/test_item_validation.py
+++ superlists/functional_tests/test_item_validation.py
@@ -1,10 +1,8 @@
 from .base import FunctionalTest
 from selenium.webdriver.common.keys import Keys
-from unittest import skip
 
 class ItemValidationTest(FunctionalTest):
 
-    @skip
     def test_cannot_add_empty(self):
         self.browser.get(self.live_server_url)
 
@@ -12,7 +10,7 @@ class ItemValidationTest(FunctionalTest):
 
         error_text = 'Empty list item not allowed'
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has_error').text,
+            self.browser.find_element_by_css_selector('.has-error').text,
             error_text
         ))
 
@@ -24,7 +22,7 @@ class ItemValidationTest(FunctionalTest):
         self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has_error').text,
+            self.browser.find_element_by_css_selector('.has-error').text,
             error_text
         ))
 
@@ -33,4 +31,4 @@ class ItemValidationTest(FunctionalTest):
         self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         self.check_row(1, item_text_A)
-        self.check_row(2, item_text_B)
+        self.check_row(3, item_text_B)
